7f3719f72a131700e3811133b6954e31f3990718
Wiktoria Demir <wiktoriaurszulademir@gmail.com>
2022-04-13 11:19:36 +0200

    13/04/2022

---
+++ OOP Python/ExercisesLastModule.py
@@ -0,0 +1,7 @@
+import utils
+
+numbers = [10, 2, 3, 12]
+# max = utils.find_max(numbers)
+
+#same thing
+print(max(numbers))
---
+++ OOP Python/Modules.py
@@ -0,0 +1,9 @@
+#Importing module
+
+import converters
+
+#import only a specific function from that module
+from converters import kg_to_lbs
+
+print(converters.lbs_to_kg(166))
+print(converters.kg_to_lbs(70))
